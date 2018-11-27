9da667bd47a427d57182526a2366ef1253b75707
serhii shlapak <shsergiy121197@gmail.com>
2018-11-27 11:20:16 +0200

    initial commit

---
+++ build/CMakeFiles/test_m.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_m.dir/src/main.cpp.o"
+  "CMakeFiles/test_m.dir/src/Move.cpp.o"
+  "libtest_m.pdb"
+  "libtest_m.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_m.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
